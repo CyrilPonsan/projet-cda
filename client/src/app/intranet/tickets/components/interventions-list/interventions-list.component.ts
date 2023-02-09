484a354cf7822cb1e804fdbfc869567bb997184b
CyrilPonsan <cyril.ponsan@gmail.com>
2023-02-09 08:38:47 +0100

    intervention list

--- client/src/app/intranet/tickets/components/interventions-list/interventions-list.component.ts
+++ client/src/app/intranet/tickets/components/interventions-list/interventions-list.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Intervention } from '../../utils/models/models';
 
 @Component({
   selector: 'app-interventions-list',
   templateUrl: './interventions-list.component.html',
-  styleUrls: ['./interventions-list.component.scss']
+  styleUrls: ['./interventions-list.component.scss'],
 })
 export class InterventionsListComponent {
-
+  @Input() interventions!: Array<Intervention>;
 }
