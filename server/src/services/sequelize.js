c30ca68955a19c7b20d87634cf4edbedecb1cbd6
leo <mxmleosallier@gmail.com>
2023-03-02 10:26:45 +0100

    windows config

--- server/src/services/sequelize.js
+++ server/src/services/sequelize.js
@@ -23,7 +23,10 @@ if (process.env.NODE8ENV === "development") {
     {
       define: { freezeTableName: true },
       host: process.env.DB_HOST,
-      dialect: "mysql",
+      dialect: "mariadb",
+      dialectOptions: {
+        socketPath: {host : "localhost", port : 3308},
+      },
       logging: false,
     }
   );
@@ -36,7 +39,10 @@ if (process.env.NODE8ENV === "development") {
     {
       define: { freezeTableName: true },
       host: process.env.DB_HOST,
-      dialect: "mysql",
+      dialect: "mariadb",
+      dialectOptions: {
+        socketPath: {host : "localhost", port : 3308},
+      },
       logging: false,
     }
   );
